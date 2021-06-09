35811951b515b828439160d0fb09533f659c172c
LeoLee <512240816@qq.com>
2021-06-10 00:08:44 +0800

    document的增删改查

--- src/test/java/org/leolee/es/springdatademo/SpringDataEsApplicationTests.java
+++ src/test/java/org/leolee/es/springdatademo/SpringDataEsApplicationTests.java
@@ -1,12 +1,20 @@
 package org.leolee.es.springdatademo;
 
 import org.junit.jupiter.api.Test;
+import org.leolee.es.springdatademo.dao.ProductDao;
 import org.leolee.es.springdatademo.entity.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class SpringDataEsApplicationTests {
@@ -15,6 +23,9 @@ public class SpringDataEsApplicationTests {
 
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Autowired
+    private ProductDao productDao;
 
     @Test
     public void contextLoads() {
@@ -34,5 +45,60 @@ public class SpringDataEsApplicationTests {
         logger.info("delete index by id:{}", s);
     }
 
+    @Test
+    public void createDocument() {
+        Product product = new Product(10000L, "iphone12", "手机", 8000.00, "http://www.baidu.com/iphone12.jpg");
+        Product save = productDao.save(product);
+        logger.info("{}", save.toString());
 
+        /*List<Product> productList = new ArrayList<>();
+        Iterable<Product> products = productDao.saveAll(productList);*/
+    }
+
+
+    @Test
+    public void updateDocumentById() {
+        //如果document的id相同，则为update操作
+        Product product = new Product(10000L, "iphone11", "手机", 5000.00, "http://www.baidu.com/iphone11.jpg");
+        Product save = productDao.save(product);
+        logger.info("{}", save.toString());
+    }
+
+    @Test
+    public void getDocumentById() {
+        Optional<Product> obj = productDao.findById(10000L);
+        logger.info("{}", obj.get());
+    }
+
+    @Test
+    public void getAllDocument() {
+        Iterable<Product> all = productDao.findAll();
+        for (Product product : all) {
+            logger.info("{}", product);
+        }
+    }
+
+    @Test
+    public void existDocument() {
+        boolean b = productDao.existsById(10000L);
+        logger.info("exist this document:{}", b);
+    }
+
+    @Test
+    public void delDocument() {
+        //productDao.deleteAll();
+        productDao.deleteById(10000L);
+    }
+
+    @Test
+    public void searchDocumentByPage() {
+        //分页&排序
+        Sort idDesc = Sort.by(Sort.Direction.DESC, "id");
+        //page-0(页码从0开始) pagesize-5
+        PageRequest pageRequest = PageRequest.of(0, 5, idDesc);
+        Page<Product> all = productDao.findAll(pageRequest);
+        for (Product product : all.getContent()) {
+            logger.info("{}", product);
+        }
+    }
 }
